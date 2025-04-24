dd1ad02b8c1324b257c5db8162922db7d671ceb4
berber <git@zmberber.com>
2025-04-24 16:13:08 +0200

    first (not working yet)

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    disko = {
+      url = "github:nix-community/disko/latest";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = (
+    inputs@{
+      self,
+      nixpkgs,
+      ...
+    }:
+    let
+      tools = import ./tools {lib = nixpkgs.lib;};
+    in
+    {
+      nixosModules = tools.setFromDir ./modules;
+      nixosConfigurations = builtins.listToAttrs (
+        map ( machine: {
+          name = machine;
+          value = nixpkgs.lib.nixosSystem (
+            let
+              data = import ./machines/${machine};
+            in {
+              specialArgs = {
+                inherit
+                  inputs
+                  data
+                  tools
+                ;
+                flake-self = self;
+              };
+              system = data.system;
+              modules = [
+                ./machines/${machine}/configuration.nix
+                inputs.disko.nixosModules.disko
+                ./machines/${machine}/hardware-configuration.nix
+                ./machines/${machine}/disk.nix
+                { imports = builtins.attrValues self.nixosModules; }
+              ];
+            }
+          );
+        }) (builtins.attrNames (
+          nixpkgs.lib.attrsets.filterAttrs (name: type: type == "directory") (builtins.readDir ./machines)
+        ))
+      );
+    }
+  );
+}
